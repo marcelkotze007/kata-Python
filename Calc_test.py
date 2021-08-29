f1e52174ededb85623a9aaac2c0ea5393c45aaff
Marcel Kotze <marcelkotze95@gmail.com>
2021-08-29 14:27:18 +0200

    feat: Passed test 3 and 4

--- Calc_test.py
+++ Calc_test.py
@@ -9,5 +9,11 @@ class AddTests(unittest.TestCase):
     def test_One(self):
         self.assertEqual(1, Calc("1"))
 
+    def test_Two(self):
+        self.assertEqual(3, Calc("1,2"))
+
+    def test_Unkown(self):
+        self.assertEqual(10, Calc("1,2,3,4"))
+
 if __name__ == '__main__':
     unittest.main()
--- String_Calc.py
+++ String_Calc.py
@@ -1,6 +1,13 @@
 def Calculator(data):
-    
+    answer = 0
+    values = []
+
     if data == "":
         return 0
     
-    return int(data)
+    values = data.split(',')
+
+    for i in values:
+        answer = answer + int(i)
+
+    return answer
